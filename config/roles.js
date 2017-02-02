eddf0cf880d0aefbd7cab777c21c5e9712d6ff16
Alina Loi <alina.loi@dev-pro.net>
2017-02-02 19:09:37 +0200

    Recompile nested roles into plain structure before exports

--- config/roles.js
+++ config/roles.js
@@ -3,14 +3,26 @@
 const roles = {
     default: 'user',
     admin: {
-        includes: ['moderator', 'user'],
+        extends: ['moderator'],
     },
     moderator: {
-        includes: ['user'],
+        extends: ['user'],
     },
     user: {
-        includes: [],
+        extends: [],
     },
 };
+
+const addNested = (target, toAdd) => {
+    if (!roles[target].extends || !roles[target].extends.length) return;
+    if (!roles[toAdd].extends || !roles[toAdd].extends.length
+        && roles[target].extends.indexOf(toAdd) === -1) roles[target].extends.push(toAdd);
+    roles[toAdd].extends.forEach((nested) => addNested(target, nested));
+};
+
+Object.keys(roles).forEach((roleName) => {
+    roles[roleName].extends && roles[roleName].extends.length
+    && roles[roleName].extends.forEach((nested) => addNested(roleName, nested))
+});
 
 module.exports = roles;
