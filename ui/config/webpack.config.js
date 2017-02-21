6b941c1ef97a09d381a7b9704c4396bf6cc52ef3
Alina Loi <alina.loi@dev-pro.net>
2017-02-21 22:32:02 +0200

    Uglify only for production build

--- ui/config/webpack.config.js
+++ ui/config/webpack.config.js
@@ -13,14 +13,14 @@ module.exports = {
         filename: 'app.js',
     },
 
-    plugins: [
+    plugins: process.env.NODE_ENV === 'production' ? [
         new webpack.DefinePlugin({
             'process.env': {
-                NODE_ENV: JSON.stringify(process.env.NODE_ENV || 'production')
+                NODE_ENV: 'production'
             }
         }),
         new webpack.optimize.UglifyJsPlugin({}),
-    ],
+    ] : [],
 
     node: {
         __dirname: false,
